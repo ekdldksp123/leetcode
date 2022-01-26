8a2a038abf6bc665324e6b9f201c801327bddf5e
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-01-26 22:52:41 +0900

    Time: 119 ms (13.48%), Space: 38.4 MB (73.04%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -3,16 +3,17 @@
  * @return {number[][]}
  */
 const generate = (numRows) => {
-    const arr = [];
+    const arr = []
+    
     for(let i = 0; i < numRows; i++) {
-        const row = [];
+        const row = []
         for(let j = 0; j <= i; j++) {
-            if(j === 0) row.push(1);
-            else if( j === i ) row.push(1);
-            else if(i > 0 && j > 0) row.push(arr[i-1][j-1] + arr[i-1][j]);
+            if(j === 0) row.push(1)
+            else if( j === i ) row.push(1)
+            else if( i > 0 && j > 0 ) row.push(arr[i-1][j-1] + arr[i-1][j])
             
         }
-        arr.push(row);
+        arr.push(row)
     }
     return arr;
 };
