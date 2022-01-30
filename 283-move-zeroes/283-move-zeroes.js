4d822e2dc0df73f21dbc04ac984beb4c77575d56
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-01-30 21:14:21 +0900

    Time: 216 ms (10.46%), Space: 47.5 MB (5.14%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,13 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const moveZeroes = (nums) => {
-    let count=0;
-    while(nums.includes(0)) {
-        nums.splice(nums.indexOf(0), 1);
-        count++;
-    }
-    for(let i = 0; i < count; i++) {
-        nums.push(0);
+    for(e of nums) {
+        if(e === 0) {
+            nums.splice(nums.indexOf(e),1);
+            nums.push(0);
+        }
     }
     return nums;
 };
