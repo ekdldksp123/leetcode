9209ad061e0ba9b1e0ffddb2525f69c87618146f
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-21 20:53:55 +0900

    Time: 75 ms (85.90%), Space: 44.2 MB (25.18%) - LeetHub

--- 202-happy-number/202-happy-number.js
+++ 202-happy-number/202-happy-number.js
@@ -4,11 +4,10 @@
  */
 const isHappy = (n) => {
     let set = new Set();
-    let sum = 0;
     
     while(true) {
         const arr = [...String(n)];
-        sum = arr.reduce((acc, cur) => {return acc + Math.pow(parseInt(cur), 2)}, 0);
+        const sum = arr.reduce((acc, cur) => {return acc + Math.pow(parseInt(cur), 2)}, 0);
         if(sum === 1) return true;
         if(set.has(sum)) return false;
         set.add(sum);
