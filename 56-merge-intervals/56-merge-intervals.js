8f9f90a3fea5cab1a8a9b06556eec122881449d1
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-27 22:45:38 +0900

    Time: 92 ms (97.16%), Space: 48.3 MB (57.88%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -9,7 +9,7 @@ const merge = (intervals) => {
     for(const current of intervals) {
         if(result.length === 0 || result[result.length-1][1] < current[0]) {
             result.push(current);
-        } else if(result[result.length-1][1] >= current[0] || result[result.length-1][1] > current[0]) {
+        } else if(result[result.length-1][1] >= current[0]) {
             result[result.length-1][1] = Math.max(current[1], result[result.length-1][1]);
         }
     }
@@ -19,5 +19,5 @@ const merge = (intervals) => {
 /*
 * 1. 먼저 intervals 를 sort (이때 [0] 만 비교한다)
 * 2. result 가 empty 이면 push + current[0] > prev[1] 이면 push
-* 3. prev[0] <= current[1] 이면 result[result.length - 1] = Math.max(current[1],result[result.length]);
+* 3. prev[1] >= current[0] 이면 result[result.length - 1] = Math.max(current[1],result[result.length]);
 */
