c72d5e091c0ad869409585d08e7e8a5c8fa8e9cf
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-28 16:11:40 +0900

    Time: 8286 ms (6.75%), Space: 87.4 MB (5.40%) - LeetHub

--- 1487-making-file-names-unique/1487-making-file-names-unique.js
+++ 1487-making-file-names-unique/1487-making-file-names-unique.js
@@ -6,7 +6,7 @@ const getFolderNames = (names) => {
     const result = [];
     const map = new Map();
     
-    for(const name of names) {
+    names.forEach((name) => {
         if(!map.has(name)) {
             map.set(name, 1);
             result.push(name);
@@ -15,20 +15,21 @@ const getFolderNames = (names) => {
             let newName =`${name}(${count})`;
             
             while(map.has(newName)) {
-                newName = `${name}(${++count})`;
+                newName = `${name}(${count++})`;
             }
             result.push(newName);
             map.set(newName, 1);
         }
-    }
+    })
+        
     return result;
 };
     
 /*
 * 1. map 에 이름이 없으면 map 에 key를 이름으로 value를 1 로 저장
 * 2. map 에 이름이 있으면 newName = `${name}(${map.get(name)})`
-* 3. newName 이 map 에 없으면 map.set(newName, 1)
-* 4. newName 이 map 에 있으면 newName = `{newName}(${map.get(${newName})}` 하고 map.set(newName, map.get(newName)+1);
+* 3. newName 이 map 에 있으면 없을때까지 newName = `${name}(${map.get(name)++})`
+* 4. while 문을 빠져나오면 map.set(newName, map.get(newName)+1);
 *
 ["kaido","kaido(1)","kaido","kaido(1)"]
 ["gta","gta(1)","gta","avalon"]
