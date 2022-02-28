5c6ac019700e015d15006c46078e5d03c613d027
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-28 13:57:19 +0900

    Time: 6879 ms (14.86%), Space: 87.1 MB (5.40%) - LeetHub

---
+++ 1487-making-file-names-unique/1487-making-file-names-unique.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} names
+ * @return {string[]}
+ */
+const getFolderNames = (names) => {
+    const result = [];
+    const map = new Map();
+    
+    for(const name of names) {
+        if(!map.has(name)) {
+            map.set(name, 1);
+            result.push(name);
+        } else {
+            let count = map.get(name);
+            let newName =`${name}(${count})`;
+            
+            while(map.has(newName)) {
+                newName = `${name}(${++count})`;
+            }
+            result.push(newName);
+            map.set(newName, 1);
+        }
+    }
+    return result;
+};
+    
+/*
+* 1. map 에 이름이 없으면 map 에 key를 이름으로 value를 1 로 저장
+* 2. map 에 이름이 있으면 newName = `${name}(${map.get(name)})`
+* 3. newName 이 map 에 없으면 map.set(newName, 1)
+* 4. newName 이 map 에 있으면 newName = `{newName}(${map.get(${newName})}` 하고 map.set(newName, map.get(newName)+1);
+*
+["kaido","kaido(1)","kaido","kaido(1)"]
+["gta","gta(1)","gta","avalon"]
+["wano","wano","wano","wano"]
+["pes","fifa","gta","pes(2019)"]
+*/
