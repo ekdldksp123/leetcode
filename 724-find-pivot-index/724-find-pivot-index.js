4fc0412b595876c181a6275d412c845537bce2a9
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-03-07 00:16:29 +0900

    Time: 83 ms (85.81%), Space: 44.5 MB (40.22%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,24 +3,21 @@
  * @return {number}
  */
 const pivotIndex = (nums) => {
-    let pivot = -1;
-    for(let i=0; i<nums.length; i++) {
-        
-        const left = nums.slice(0, i).reduce((acc, cur) => acc+cur,0) || 0;
-        const right = nums.slice(i + 1).reduce((acc, cur) => acc+cur,0) || 0;
-        
-        if(left === right) {
-            pivot = i; 
-            break;
-        }
+    const sum = nums.reduce((acc, cur) => acc+cur,0);
+    let left = 0;
+    
+    for(let i=0; i < nums.length; i++) {
+        left += i > 0 ? nums[i-1] : 0;
+        if(left === (sum - left - nums[i])) return i;
     }
-    return pivot;
+    return -1;
 };
 
 
 /*
-* 1. 배열의 length % 2 !== 0 이면 -1을 리턴
-* 2. forEach 문을 돌면서 현재 index 가 (arr.length/2) - 1 일때 앞뒤로 slice 해서 sum 비교
-* 3. 값이 같으면 index 리턴
+*
+*
+*
+*
 *
 */
