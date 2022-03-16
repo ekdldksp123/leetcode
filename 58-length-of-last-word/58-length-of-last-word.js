dcc196c4cb805b3952f956b57206201ae189a38a
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-03-16 19:00:52 +0900

    Time: 101 ms (24.67%), Space: 41.9 MB (63.56%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = (s) => {
+    const arr = s.split(' ').filter(e => e!== '');
+    return arr[arr.length -1].length;
+}
+
+/*
+* 1. 공백을 중심으로 split 한다
+* 2. '' 이 아닌 마지막 요소를 찾는다
+* 3. 마지막 글자의 길이를 구한다
+*/
