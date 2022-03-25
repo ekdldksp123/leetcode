761a3255c6faad88b06ee0e9bdf6d4b5b7ac7815
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-03-25 11:42:10 +0900

    Time: 784 ms (5.05%), Space: 86.6 MB (5.14%) - LeetHub

---
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+const getSmallestString = function(n, k) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    
+    for(let i = n; i > 0; i--) {
+        const num = k - n;
+        if(num >= 26) {
+            result += alphabet[25];
+            k -= 25
+        } else {
+            result += alphabet[num];
+            k -= num
+        }
+    }
+    
+    return result.split('').reverse().join('')
+}
+
+
+/*
+* 1. Array.from({ length: 26 }, (v,i) => i + 97).join('') => const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+* 2. n 만큼 반복하면서(*) k-n >= 26 일 경우 z => z의 개수부터(**) 구해야 함
+* 3. k-n < 26 일 경우 a <= ? < z 의 ?를 구해야 함
+* 4. return 시에 str 반대로 뒤집어주기
+*/
