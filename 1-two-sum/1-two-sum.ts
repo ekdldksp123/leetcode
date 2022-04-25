6190103f0c59721c6c3effe14a2cf2e69a0889fc
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-04-25 14:25:29 +0900

    Time: 207 ms (18.20%), Space: 44.7 MB (72.70%) - LeetHub

---
+++ 1-two-sum/1-two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    const hashMap: Record<string, number> = {};
+    
+    for(const e of nums) {
+        const findValue = target - e
+        const currentIndex = nums.lastIndexOf(e)
+        
+        if(findValue in hashMap) return [nums.indexOf(findValue), currentIndex]
+        
+        hashMap[e] = e
+    }
+};
