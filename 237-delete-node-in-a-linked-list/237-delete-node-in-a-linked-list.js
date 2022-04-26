48c43323d59978e82aa7b73a989086c0fc44d735
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-04-26 16:53:45 +0900

    Time: 83 ms (61.23%), Space: 43.7 MB (97.36%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
@@ -13,3 +13,8 @@ const deleteNode = (node) => {
     node.val = node.next.val
     node.next = node.next.next
 };
+
+/*
+* 1. 이미 node 주소값은 제거해야 할 val 이 속한 노드리스트 전체의 주소값을 바라보고 있기 때문에
+* 2. node = node.next 를 해도 주소값은 변하지 않는다
+*/
