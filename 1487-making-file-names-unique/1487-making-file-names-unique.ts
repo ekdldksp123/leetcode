735f8fd09681d9b12aaffa1182d9a97019bb801a
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-04-26 13:44:18 +0900

    Time: 6957 ms (33.33%), Space: 88.2 MB (16.67%) - LeetHub

--- 1487-making-file-names-unique/1487-making-file-names-unique.ts
+++ 1487-making-file-names-unique/1487-making-file-names-unique.ts
@@ -1,9 +1,11 @@
 function getFolderNames(names: string[]): string[] {
   const map = new Map()
+  const res: string[] = []
     
   for (const name of names) {
     if (!map.has(name)) {
       map.set(name, 1)
+      res.push(name)
     } else {
       let length = map.get(name)
       let newName = `${name}(${length})`
@@ -11,7 +13,8 @@ function getFolderNames(names: string[]): string[] {
         newName = `${name}(${length++})`
       }
       map.set(newName, 1)
+      res.push(newName)
     }
   }
-  return [...map.keys()]
+  return res
 }
