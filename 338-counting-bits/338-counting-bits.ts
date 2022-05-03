e16861daadac6ebe33972a3a19b2f97654d864f1
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-05-03 09:13:17 +0900

    Time: 118 ms (70.00%), Space: 51.3 MB (30.00%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.ts
@@ -0,0 +1,11 @@
+const countBits = (n : number) => {
+  return Array.from({length: n + 1}, (v,i) => i).map(v => countChar(v.toString(2)))
+}
+
+function countChar(str: string): number {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '1') count++
+    }
+    return count
+}
