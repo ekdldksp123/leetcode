fb55f07796d14e1bc3dcd3900768092296b022fb
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-05-03 09:05:38 +0900

    Time: 162 ms (20.72%), Space: 62.4 MB (5.11%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -6,8 +6,15 @@ const countBits = (n) => {
     const result = []
     const arr = Array.from({length: n + 1}, (v,i) => i).map(v => v.toString(2))
     
-    for(const e of arr) {
-        result.push([...e].filter(char => char === '1').length)
+    for(let i=0; i<arr.length; i++) {
+        const map = new Map()
+        let cnt = map.get(`${i}`) || 0
+        
+        for(const el of [...arr[i]]) {
+            
+            if(el === '1') map.set(`${i}`, cnt++)
+        }
+        result.push(cnt)
     }
     
     return result
