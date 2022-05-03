6a7e7953dc092bb9f6c64c7ed1e9b9e2b9d56446
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-05-03 15:59:31 +0900

    Time: 169 ms (8.83%), Space: 50.1 MB (5.11%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+
+
+
+const isIsomorphic = (s, t) => {
+    const map = new Map()
+    
+    const sArr = [...s]
+    const tArr = [...t]
+    
+    for(let i = 0; i < s.length; i++) {
+    
+        if(!Array.from(map.keys()).includes(s[i])) map.set(s[i], i)
+        sArr[i] = map.get(s[i])
+    }
+    map.clear()
+    
+    for(let i = 0; i < t.length; i++) {
+    
+        if(!Array.from(map.keys()).includes(t[i])) map.set(t[i], i)
+        tArr[i] = map.get(t[i])
+    }
+    
+    return JSON.stringify(sArr) === JSON.stringify(tArr)
+};
+
