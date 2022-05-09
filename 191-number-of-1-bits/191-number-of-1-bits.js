499d20721b1d481a2006612b53a3cbc60f6812c3
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-05-09 09:46:52 +0900

    Time: 75 ms (72.17%), Space: 42.5 MB (69.87%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -9,7 +9,7 @@ const hammingWeight = (n) => {
     while(n > 0) {
         if(n % 2 === 1) {
             count ++
-            n = Math.floor(n / 2)
+            n = (n / 2) - 0.5
         } else n = n / 2
     }
     
