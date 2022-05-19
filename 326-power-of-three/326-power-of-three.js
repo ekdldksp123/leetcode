b478c8ff1a18fac69a2efa1d56406fdcbc3b8abc
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-05-19 19:51:32 +0900

    Time: 332 ms (34.85%), Space: 51.9 MB (5.27%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -7,7 +7,7 @@ const isPowerOfThree = (n) => {
     if(n <= 0) return false
     
     while(n > 1) {
-        if(n % 3 <= 2 && n % 3 > 0) return false
+        if(n % 3 !== 0) return false
         n = n / 3
     }
     return true
