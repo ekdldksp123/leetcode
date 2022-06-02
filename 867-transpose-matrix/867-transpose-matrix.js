ebe4d0c03b8e19cdfbc7d238ff09a0fa8ffbe276
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-06-02 19:16:06 +0900

    Time: 99 ms (39.38%), Space: 45.1 MB (21.88%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+const transpose = (matrix) => {
+    const result = Array.from({ length: matrix[0].length }, (v,i) => [])
+    
+    for(let i = 0; i < matrix[0].length; i++) {
+        for(let j = 0; j < matrix.length; j++) {
+            
+            result[i].push(matrix[j][i])
+        }
+    }
+    
+    return result
+};
