954d16c20d6bcbf489667788a693437f9667614f
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-07-20 12:25:40 +0900

    Time: 107 ms (35.95%), Space: 44.5 MB (22.66%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const intersect = function(nums1, nums2) {
+    const map = new Map()
+    for(const n of nums1) {
+        map.set(n, (map.get(n) || 0) + 1)
+    }
+    const result = [];
+    for(const n of nums2) {
+        if(map.has(n) && map.get(n) > 0) {
+            map.set(n, map.get(n)-1)
+            result.push(n)
+        }
+    }
+    return result
+};
