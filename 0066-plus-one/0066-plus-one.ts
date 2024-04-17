2ca6119fc6f9c63db392faec5297e4b5d1da34b4
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2024-04-17 22:33:19 +0900

    Time: 67 ms (12.01%), Space: 51.6 MB (18.9%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,6 +1,6 @@
 function plusOne(digits: number[]): number[] {
     const num = BigInt(digits.join(''))
     const numPlusOne = num + BigInt(1);
-    const result = [...`${numPlusOne}`].map(v => Number(v));
+    const result = numPlusOne.toString().split('').map(v => Number(v));
     return result;
 };
