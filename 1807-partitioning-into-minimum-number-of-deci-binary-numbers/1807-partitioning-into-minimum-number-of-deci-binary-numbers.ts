325aedbdf419ee0c74226f20880389a37db2db6d
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2024-06-17 22:41:29 +0900

    Time: 97 ms (10.58%), Space: 55.1 MB (59.62%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.ts
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.ts
@@ -1,12 +1,13 @@
 /*
  1. deci-binary = 10, 11, 100, 101, 111, ... 
  2. 각 자리수마다 1씩 뺄수 있으므로 가장 큰 자릿수를 구하면 됨!
+ 3. 숫자로 형변환하는 건 왜 parseInt가 제일 빠른거지?
  */
 
 const minPartitions = (n: string): number => {
     let result = 0
-    for(const digit of n) {
-        result = Math.max(result, parseInt(digit))
+    for(let i=0; i<n.length; i++) {
+        result = Math.max(result, parseInt(n[i]))
     }
     return result
 };
