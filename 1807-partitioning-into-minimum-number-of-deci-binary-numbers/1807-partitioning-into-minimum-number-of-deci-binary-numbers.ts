ef1c67c574329e9243a381e0c82409c1201ab001
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2024-06-17 22:40:29 +0900

    Time: 119 ms (5.77%), Space: 54.9 MB (64.42%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.ts
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.ts
@@ -1,13 +1,12 @@
 /*
  1. deci-binary = 10, 11, 100, 101, 111, ... 
  2. 각 자리수마다 1씩 뺄수 있으므로 가장 큰 자릿수를 구하면 됨!
- 3. 새로운 치환 방법: +"문자열" => 숫자 
  */
 
 const minPartitions = (n: string): number => {
     let result = 0
-    for(let i=0; i<n.length; i++) {
-        result = Math.max(result, +n[i])
+    for(const digit of n) {
+        result = Math.max(result, Number(digit))
     }
     return result
 };
