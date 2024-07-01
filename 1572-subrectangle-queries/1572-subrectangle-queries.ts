77ead5be93d74cd8f80b393012dd551e5d1e225c
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2024-07-01 09:07:34 +0900

    Time: 86 ms (42.65%), Space: 56.2 MB (97.06%) - LeetHub

---
+++ 1572-subrectangle-queries/1572-subrectangle-queries.ts
@@ -0,0 +1,26 @@
+class SubrectangleQueries {
+    rectangle: number[][] = []
+
+    constructor(rectangle: number[][]) {
+        this.rectangle = rectangle
+    }
+
+    updateSubrectangle(row1: number, col1: number, row2: number, col2: number, newValue: number): void {
+        for (let row = row1; row <= row2; row++) {
+            for (let col = col1; col <= col2; col++) {
+                this.rectangle[row][col] = newValue
+            }
+        }
+    }
+
+    getValue(row: number, col: number): number {
+        return this.rectangle[row][col]
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = new SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+ * var param_2 = obj.getValue(row,col)
+ */
