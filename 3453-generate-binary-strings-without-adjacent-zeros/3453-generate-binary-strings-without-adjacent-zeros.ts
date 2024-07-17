0b67e8b7e84b4d2172ce16f00449f8357fb42f10
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2024-07-17 22:46:49 +0900

    Time: 70 ms (56.9%), Space: 54.8 MB (93.1%) - LeetHub

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.ts
@@ -0,0 +1,18 @@
+const validStrings = (n: number): string[] => {
+    const result: string[] = []
+    
+    const backtrack = (current: string, lastChar: string) => {
+        if (current.length === n) {
+            result.push(current)
+            return
+        }
+        
+        if (lastChar !== '0') {
+            backtrack(current + '0', '0')
+        }
+        backtrack(current + '1', '1')
+    }
+    backtrack('', '')
+    
+    return result;
+};
