fdbf822687a94f858c0f8158daef734f27835c98
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2024-07-31 22:01:32 +0900

    Time: 61 ms (50%), Space: 51.8 MB (33.33%) - LeetHub

--- 1342-queens-that-can-attack-the-king/1342-queens-that-can-attack-the-king.ts
+++ 1342-queens-that-can-attack-the-king/1342-queens-that-can-attack-the-king.ts
@@ -1,6 +1,7 @@
 /**
-    king의 자리에서 left, right, up, down, left-up, left-down, right-up, right-down 
+    1. king의 자리에서 left, right, up, down, left-up, left-down, right-up, right-down 
     8가지 방향으로 가장 가까운 queen의 자리를 찾는다.
+    2. Set = HashMap으로 전환하고 계산하면 훨씬 빨라진다
  */
 const directions = [
     [0, 1],  // right
@@ -15,13 +16,14 @@ const directions = [
 
 const queensAttacktheKing = (queens: number[][], king: number[]): number[][] => {
     const result = []
+    const queensLocationSet = new Set(queens.map(([x,y]) =>`${x}${y}`))
     for (const [dx, dy] of directions) {
         let [x, y] = king
         
         while (x >= 0 && x < 8 && y >= 0 && y < 8) {
             x += dx
             y += dy
-            if (queens.find(([a,b]) => a === x && b === y)) {
+            if (queensLocationSet.has(`${x}${y}`)) {
                 result.push([x, y])
                 break;
             }
