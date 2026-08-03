5e42ca96bb90513d05693d10c9aac4711aa679e7
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2026-08-03 19:21:09 +0900

    Time: 634 ms (5.06%), Space: 78.1 MB (5.06%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.ts
+++ 0006-zigzag-conversion/0006-zigzag-conversion.ts
@@ -4,13 +4,12 @@ function convert(s: string, numRows: number): string {
     const zigzag = []
     const betweenCount = numRows - 2;
     
-    const charIdx = 0;
     let rowIdx = 0;
     while (s.length) {
-        zigzag[rowIdx] = [...s.slice(charIdx, charIdx + numRows)]
-
+        zigzag[rowIdx] = [...s.slice(0, numRows)]
         rowIdx += 1
         s = s.slice(numRows)
+        
         if (betweenCount > 0 && s.length) {
             for(let i=0; i < betweenCount; i++) {
                 zigzag[rowIdx] = Array.from({length: numRows})
