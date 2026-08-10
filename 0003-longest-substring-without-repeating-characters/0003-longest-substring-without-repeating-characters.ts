71ac99b55753843a7ef7ff87c6163bc2c4c0d7fb
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2026-08-10 12:04:28 +0900

    Time: 45 ms (31.88%), Space: 60.9 MB (41.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -1,18 +1,17 @@
 function lengthOfLongestSubstring(s: string): number {
-    let arr: boolean[] = Array.from({length: 128}, () => false)
-    let l = 0;
-    let r = 0;
     let maxLen = 0;
+    let charMap = new Map();
 
-    while(r < s.length) {
-        if(!arr[s.charCodeAt(r)]) {
-            arr[s.charCodeAt(r)] = true;
-            maxLen = Math.max(maxLen, r - l + 1)
-            r++;
-        } else {
-            arr[s.charCodeAt(l)] = false;
-            l++;
+    let left = 0;
+    for(let right=0; right < s.length; right++) {
+        const char = s[right];
+        if(charMap.has(char) && charMap.get(char) >= left) {
+            left = charMap.get(char) + 1;
         }
+        maxLen = Math.max(maxLen, right - left + 1);
+
+        charMap.set(char, right);
     }
-    return maxLen
+
+    return maxLen;
 };
