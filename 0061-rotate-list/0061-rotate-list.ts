1a85126a17d947000bb2191457c649595205a445
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2026-09-18 12:39:33 +0900

    Time: 0 ms (100%), Space: 58.2 MB (56.67%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.ts
+++ 0061-rotate-list/0061-rotate-list.ts
@@ -11,31 +11,30 @@
  */
 
 function rotateRight(head: ListNode | null, k: number): ListNode | null {
-    if(!head || !head.next || k === 0) return head
+    if(head === null || head.next === null || k === 0) return head
 
-    let length = 1
-    let last = head
-    while(last.next) {
-        length ++
-        last = last.next
+    let n = 0
+    let curr: ListNode | null = head
+    let last: ListNode | null = null
+
+    while(curr) {
+        if(curr.next === null) {
+            last = curr
+        }
+        curr = curr.next
+        n++
     }
 
-    k %= length
+    k = k % n
     if(k === 0) return head
+    curr = head
 
-    let curr = head
-    let count = 1
-    while(count < length - k) {
-        curr = curr.next!
-        count++
+    for(let i=0; i < n - k - 1; i++) {
+        curr = curr.next
     }
 
-    // curr.next = 실제 회전해서 앞으로 가는 부분. val 은 버림
     let newHead = curr.next
-    // curr의 next 를 null로 할당함으로써 newHead 부분을 잘라내고 head 에서는 newHead를 잘라냄
     curr.next = null
-    // 마지막 요소에 회전된 뒷 부분을 연결
     last.next = head
-    // last(회전부 연결), curr(회전 주체), newHead(회전된 앞부분) 모두 head를 참조하고 있기에 연결되서 리턴
     return newHead
 };
